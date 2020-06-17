996aec11b4b59aea72571387fa659223e3c6c9ca
Jarno Westhof <jarno.westhof@wolterskluwer.com>
2020-06-17 15:47:41 +0200

    more tooltips, included twinfield tooltips, update other site tooltips

--- Site/Hovercraft/Hovercraft/Controllers/TooltipsController.cs
+++ Site/Hovercraft/Hovercraft/Controllers/TooltipsController.cs
@@ -21,6 +21,8 @@ namespace Hovercraft.Controllers
 				return GetDummyPageTooltips();
 			if (website.Contains("ospokolenko.wixsite.com/nonsense"))
 				return GetOspokolenkoWixSiteTooltips();
+			if (website.Contains("accounting.twinfield.com"))
+				return GetTwinfieldSalesToolTips();
 
 			return new TooltipLocatorModel[0];
 		}
@@ -78,6 +80,148 @@ namespace Hovercraft.Controllers
 						Lines = new []
 						{
 							"Join the most forward-thinking accountants and let them share the experience"
+						}
+					}
+				},
+				new TooltipLocatorModel
+				{
+					Locator = @"#comp-kbjc9eya0label",
+					Tooltip = new TooltipModel
+					{
+						Header = "Home",
+						Lines = new [] { "Here you can find out all latest accounting information" }
+					}
+				},
+				new TooltipLocatorModel
+				{
+					Locator = @"#comp-kbjc9eya1linkElement",
+					Tooltip = new TooltipModel
+					{
+						Header = "Donation",
+						Lines = new [] { "If you feel inspired about our web-site, you are welcome to help us" }
+					}
+				},
+				new TooltipLocatorModel
+				{
+					Locator = @"#comp-kbjc9eya2label",
+					Tooltip = new TooltipModel
+					{
+						Header = "About",
+						Lines = new [] { "The story about our members" }
+					}
+				},
+				new TooltipLocatorModel
+				{
+					Locator = @"#comp-kbjc9eya3label",
+					Tooltip = new TooltipModel
+					{
+						Header = "Blog",
+						Lines = new [] { "Join the most forward-thinking accountants and let them share the experience" }
+					}
+				},
+				new TooltipLocatorModel
+				{
+					Locator = @"#comp-kbjc9eya4label",
+					Tooltip = new TooltipModel
+					{
+						Header = "Yelp Review",
+						Lines = new [] { "Check comments and starts on Yelp" }
+					}
+				},
+				new TooltipLocatorModel
+				{
+					Locator = @"#comp-kbjc9eya5label",
+					Tooltip = new TooltipModel
+					{
+						Header = "Knowledge Base",
+						Lines = new [] { "Check out the accounting syllabus" }
+					}
+				},
+			};
+		}
+
+		IEnumerable<TooltipLocatorModel> GetTwinfieldSalesToolTips()
+		{
+			return new[]
+			{
+				new TooltipLocatorModel
+				{
+					Locator = @"div.flex-list-container div:contains('Irfan Coleman')",
+					Tooltip = new TooltipModel
+					{
+						Lines = new []
+						{
+							"Not Here 345",
+							"4626 ;D",
+							"Kiev - Ukraine",
+							"687-628785174",
+							"icoleman@wolterskluwer.com"
+						}
+					}
+				},
+				new TooltipLocatorModel
+				{
+					Locator = @"div.flex-list-container div:contains('108.000,00')",
+					Tooltip = new TooltipModel
+					{
+						Header = "Items",
+						Lines = new []
+						{
+							"2x Pet Dragon €54000,00",
+						}
+					}
+				},
+				new TooltipLocatorModel
+				{
+					Locator = @"div .flex-list-container div:contains('Quentin Weaver')",
+					Tooltip = new TooltipModel
+					{
+						Lines = new []
+						{
+							"Australishestraat 8925",
+							"6395 :D",
+							"Amersfoort - Netherlands",
+							"qweaver@wolterskluwer.com"
+						}
+					}
+				},
+				new TooltipLocatorModel
+				{
+					Locator = @"div .flex-list-container div:contains('100.000,00')",
+					Tooltip = new TooltipModel
+					{
+						Header = "Items",
+						Lines = new []
+						{
+							"1x WK Code Games Victory €100000,00",
+						}
+					}
+				},
+				new TooltipLocatorModel
+				{
+					Locator = @"div .flex-list-container div:contains('Leia Mcgregor')",
+					Tooltip = new TooltipModel
+					{
+						Lines = new []
+						{
+							"Somewhere Nearby 40",
+							"3563264 ZIP",
+							"Knowhere - Netherlands",
+							"06-3095783545",
+							"admin@leiamcgregor.com"
+						}
+					}
+				},
+				new TooltipLocatorModel
+				{
+					Locator = @"div .flex-list-container div:contains('78.050,00')",
+					Tooltip = new TooltipModel
+					{
+						Header = "Items",
+						Lines = new []
+						{
+							"1x Electric Car €78.000,00",
+							"5x Pen 50,00"
 						}
 					}
 				}
